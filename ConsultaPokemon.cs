bf225e49e0a86e4b8a535f86242590c7605ad6de
Gabriel <Gabriel@DESKTOP-7ETD8RD>
2023-09-20 21:28:11 -0300

    Adicionar arquivos de projeto.

---
+++ ConsultaPokemon.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace Pokemon_Tamagochi
+{
+    public class ConsultaPokemon
+    {
+
+            public static string ConsultarPokemon(string nomePokemon)
+            {
+                var pokemon = nomePokemon;
+                var client = new RestClient($"https://pokeapi.co/api/v2/pokemon/{pokemon}/");
+                RestRequest request = new RestRequest("", Method.Get);
+                // execução do método GET
+
+                var response = client.Execute(request);
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    return response.Content;
+                }
+                else
+                {
+                    return response.ErrorMessage;
+                }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using Pokemon_Tamagochi;
+using RestSharp;
+
+Console.WriteLine("Procurando Pokemon! \n");
+
+Console.WriteLine("Digite o Pokemon que deseja escolher: \n");
+Console.WriteLine("1 - Squirtle");
+Console.WriteLine("2 - Charmander");
+Console.WriteLine("3 - Bulbasaur");
+Console.WriteLine("0 - Finalzar programa! \n");
+
+var opcaoPokemon = Console.ReadLine();
+var resultado = "";
+switch (opcaoPokemon)
+{
+    case "1":
+        resultado = ConsultaPokemon.ConsultarPokemon("squirtle");
+        break;
+    case "2":
+        resultado = ConsultaPokemon.ConsultarPokemon("charmander");
+        break;
+    case "3":
+        resultado = ConsultaPokemon.ConsultarPokemon("bulbasaur");
+        break;
+    default:
+        break;
+}
+
+Console.WriteLine(resultado);
+
+
+Console.WriteLine("Programa finalizado, busca concluída! \n");
+
+
+
+public class Pokemon
+{
+    public string count { get; set; }
+
+    public string next { get; set; }
+
+    public string previous { get; set; }
+
+    public string results { get; set; }
+}
